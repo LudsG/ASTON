7a548a535998924cb5275f8825cee65783abe073
Luds G <ludovic.971@hotmail.fr>
2018-05-28 16:03:18 +0200

    first commit

---
+++ script.ps1
@@ -0,0 +1,45 @@
+﻿#$fruits = @("Pomme", "Cerise", "Banane")
+#
+#foreach ($item in $fruits) {
+#    Write-Output $item;
+#}
+
+#for($i=0; $i -lt 10; $i++) {
+#    Write-Output $i;
+#    }
+
+#$i = 0;
+#
+#while ($i -lt 10) {
+#    Write-Output $i;
+#    $i++;
+#}
+
+#[string] $msg = "Je dois faire une sauvegarde"
+#
+#for ($i=0; $i -lt 500; $i++) {
+#    echo $msg;
+#    $i++;
+#    }
+
+#[int] $num = 1
+#
+#while ($num -lt 14999) {
+#    $num = $num + 2;
+#    echo $num;
+#}     
+
+#for($i=0; $i -lt 11; $i++) {
+#    13 * $i;
+#    }
+
+#$tests = Read-Host "Donne moi un mot"
+#$tests.Length
+
+$tests = Read-Host "Donne moi un nombre"
+
+if ($tests%2 -eq 0) {
+    echo "nombre pair"
+} else {
+    echo "nombre impair"
+    }
